ec197b7c17c69be46db5c6ae6f7abd4a36f6173f
siniirobot <siniirobot@gmail.com>
2018-06-29 11:01:21 +0700

    Положил в пакет Range (почему то разность выводится с обрезаной точкой)

---
+++ Range/src/Range/FindRange.java
@@ -0,0 +1,66 @@
+package Range;
+
+import java.util.Scanner;
+
+public class FindRange {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите начало диапазона первого отрезка:");
+        double firstLineBegan = scanner.nextDouble();
+
+        System.out.println("Введите конец дииапозона первого отрезка:");
+        double firstLineEnd = scanner.nextDouble();
+
+        System.out.println("Введите начало диапазона второго отрезка:");
+        double secondLineBegan = scanner.nextDouble();
+
+        System.out.println("Введите конец дииапозона второго отрезка:");
+        double secondLineEnd = scanner.nextDouble();
+
+        System.out.println("Введите число для проверки диапозона.");
+        double inRange = scanner.nextDouble();
+
+        Range range = new Range(firstLineBegan, firstLineEnd);
+
+        System.out.printf("Числовой диапозон первого отрезка равен: %f %n", range.getLength());
+
+        double[] intersection = range.getNewRange(firstLineBegan, firstLineEnd, secondLineBegan, secondLineEnd);
+
+        if (intersection == null) {
+            System.out.println("Нет пересечения");
+        }else {
+            System.out.print("Пересечение происходит в числах:");
+            for (double e:intersection) {
+                System.out.printf(" %f",e);
+            }
+        }
+
+        double[] unionRange = range.getUnionRange(firstLineBegan, firstLineEnd, secondLineBegan, secondLineEnd);
+
+        if (unionRange.length == 2) {
+            System.out.printf("%nОбьеденение делает новый отрезок с диапозонм:");
+            for (double e:unionRange) {
+                System.out.printf(" %f",e);
+            }
+        }else {
+            System.out.print("Обьеденение делает два отрезка:");
+            for (double e:unionRange) {
+                System.out.printf(" %f",e);
+            }
+        }
+
+        System.out.printf("%nРазность двух отрезков равна:");
+        for (double e:range.getDifference(firstLineBegan, firstLineEnd, secondLineBegan, secondLineEnd)) {
+            System.out.print(e + " ");
+        }
+
+        System.out.println();
+        if (range.isInRange(inRange)) {
+            System.out.println("Число в диапозоне.");
+        } else {
+            System.out.println("Число вне диапозона.");
+        }
+    }
+}
---
+++ Range/src/Range/Range.java
@@ -0,0 +1,108 @@
+package Range;
+public class Range {
+    private double from;
+    private double to;
+
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public double getFrom() {
+        return this.from;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    public double getLength() {
+        return this.to - this.from;
+    }
+
+    public boolean isInRange(double tryNumber) {
+        return from <= tryNumber && tryNumber <= to;
+    }
+
+    public double[] getNewRange (double firstLineBegan, double firstLineEnd, double secondLineBegan,double secondLineEnd) {
+
+        boolean intersection = false;
+        double intersectionPointOpen = 0;
+        double intersectionPointClose = 0;
+
+        for (double i = firstLineBegan; i <= firstLineEnd; ++i) {
+            for (double j = secondLineBegan; j <= secondLineEnd; ++j) {
+                if (!intersection) {
+                    intersectionPointOpen = j;
+                }
+                if (i == j) {
+                    intersection = true;
+                    intersectionPointClose = j;
+                    break;
+                }
+            }
+        }
+
+        if (intersection) {
+            return new double[] {intersectionPointOpen, intersectionPointClose};
+        }else {
+            return null;
+        }
+    }
+
+    public double[] getUnionRange (double firstLineBegan, double firstLineEnd, double secondLineBegan,double secondLineEnd) {
+
+        for (double i = firstLineBegan; i <= firstLineEnd; ++i) {
+            if (i == secondLineBegan){
+                return new double[] {secondLineBegan,secondLineEnd};
+            }
+        }
+        return new double[] {firstLineBegan,firstLineEnd,secondLineBegan,secondLineEnd};
+    }
+
+    public double[] getDifference (double firstLineBegan, double firstLineEnd, double secondLineBegan,double secondLineEnd) {
+
+        int arraySize = 0;
+
+        for (double i = firstLineBegan; i <= firstLineEnd; ++i) {
+            boolean intersection = false;
+            for (double j = secondLineBegan; j <= secondLineEnd; ++j) {
+                if ( i == j) {
+                    intersection = true;
+                    break;
+                }
+            }
+            if (!intersection) {
+                arraySize++;
+            }
+        }
+
+        double[] newRange = new double[arraySize];
+
+        int arrayCount = 0;
+
+        for (double i = firstLineBegan; i <= firstLineEnd; ++i) {
+            boolean intersection = false;
+            for (double j = secondLineBegan; j <= secondLineEnd; ++j) {
+                if ( i == j) {
+                    intersection = true;
+                    break;
+                }
+            }
+            if (!intersection) {
+                newRange[arrayCount] = i;
+                arrayCount++;
+            }
+        }
+
+        return newRange;
+    }
+}
