902ed2598abc511c6e05df02bfb1e4b08d1c42a1
SLG <siniirobot@gmail.com>
2018-07-26 02:22:39 +0700

    ArrayListHome Задача 1 - тут обработайте исключение если файла нет. +
    Задача 2 - есть ошибка. +

--- ArrayListHome/src/ru/academItSchool/gorbunov/main/Realization.java
+++ ArrayListHome/src/ru/academItSchool/gorbunov/main/Realization.java
@@ -7,22 +7,27 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Realization {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         try (Scanner scanner = new Scanner(new FileInputStream("try.txt"), "windows-1251")) {
             ArrayList<String> lines = new ArrayList<>();
             while (scanner.hasNextLine()) {
                 lines.add(scanner.nextLine());
             }
             System.out.println(lines);
+        } catch (FileNotFoundException e) {
+            System.out.println("Потерялся файл");
         }
 
-        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
-        for (int i = 0; i < numbers.size(); ++i) {
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16));
+        for (int i = 0; i < numbers.size(); ) {
             if (numbers.get(i) % 2 == 0) {
                 numbers.remove(i);
+                continue;
             }
+            ++i;
         }
-        System.out.println(numbers);
+
+        System.out.println("Список без четных чисел -" + numbers);
 
         ArrayList<Integer> sameNumbers = new ArrayList<>(Arrays.asList(1, 1, 2, 2, 3, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 3, 8, 9, 9, 10, 10));
         ArrayList<Integer> copySameNumber = new ArrayList<>();
@@ -31,7 +36,7 @@ public class Realization {
                 copySameNumber.add(e);
             }
         }
-        System.out.println(sameNumbers);
-        System.out.println(copySameNumber);
+        System.out.println("Изначальный список - " + sameNumbers);
+        System.out.println("Новый список без повторений" + copySameNumber);
     }
 }
