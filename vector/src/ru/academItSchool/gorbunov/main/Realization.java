e07f2400fbf48087fd6dfd3b40b25f3d7f0ae330
Горбунов Петр <40553017+siniirobot@users.noreply.github.com>
2018-07-30 18:49:01 +0700

    1. Длину не нужно хранить в поле, ее всегда можно получить у массива +
    2. Тут много где удобнее Arrays.copyOf +
    
    3. Конструкторы не должны позволять создать вектор длины 0 +
    
    4. Новый массив и так заполнен нулями +
    
    5. Нестатические сумма и разность. +
    Хочется чтобы массив прересоздавался только если это нужно.
    И сейчас неверно что длины складываются, такого не должно быть
    
    6. Умножение на скаляр. +
    Там не должен пересоздаваться вектор, надо просто поменять текущий вектор.
    Число компонент не должно меняться
    
    7. Получение длины - сейчас делает не то +
    
    8. Сеттер по индексу - обычно индекс ставят первым аргументом +
    
    9. Скалярное произведение - там надо обойтись без создания  новых векторов. +
    Результатом должно быть число

--- vector/src/ru/academItSchool/gorbunov/main/Realization.java
+++ vector/src/ru/academItSchool/gorbunov/main/Realization.java
@@ -4,10 +4,10 @@ import ru.academItSchool.gorbunov.vector.Vector;
 
 public class Realization {
     public static void main(String[] args) {
-        Vector vector1 = new Vector(1, new double[]{1, 2, 3, 4, 5});
+        Vector vector1 = new Vector(5, new double[]{1, 2, 3, 4, 5});
         System.out.println("Векстор 1 - " + vector1);
 
-        Vector vector2 = new Vector(1, new double[]{1, 2, 3, 4, 5, 6, 7});
+        Vector vector2 = new Vector(10, new double[]{1, 2, 3, 4, 5, 6, 7});
         System.out.println("Векстор 2 - " + vector2.toString());
 
         Vector vector3 = new Vector(vector1);
@@ -30,7 +30,7 @@ public class Realization {
 
         System.out.println("Длина вектора равна = " + vector1.getVectorLength());
 
-        System.out.println("По заданому индексу стоит элемент - " + vector1.getVectorElementByIndex(1));
+        System.out.println("По заданому индексу стоит элемент - " + vector1.getVectorElementByIndex(0));
 
         vector1.setVectorElementByIndex(0, 25);
         System.out.println("Теперь на указаном месте стоит указаное число - " + vector1.toString());
@@ -44,7 +44,7 @@ public class Realization {
         Vector vector4 = new Vector(5);
         System.out.println(vector4);
 
-        Vector vector5 = new Vector(new double[]{1,2,4,7});
+        Vector vector5 = new Vector(new double[]{1});
         System.out.println(vector5);
     }
 }
