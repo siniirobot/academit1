107b050d2779be0bbbadd0928769ba611d7f98d1
Горбунов Петр <40553017+siniirobot@users.noreply.github.com>
2018-08-24 20:09:54 +0700

    Persons •А) получить список уникальных имен + •Б) вывести список уникальных имен в формате: Имена: Иван, Сергей, Петр. + •В) получить список людей младше 18, посчитать для них средний возраст + •Г) при помощи группировки получить Map, в котором ключи – имена, а значения – средний возраст + •Д) получить людей, возраст которых от 20 до 45, вывести в консоль их имена в порядке убывания возраста +

--- Person/src/ru/academItSchool/gorbunov/main/Realization.java
+++ Person/src/ru/academItSchool/gorbunov/main/Realization.java
@@ -12,30 +12,32 @@ public class Realization {
                 new Person("Петр", 16),
                 new Person("Павел", 20),
                 new Person("Сергей", 11),
+                new Person("Сергей", 35),
                 new Person("Александр", 15),
+                new Person("Александр", 27),
                 new Person("Алексей", 46),
                 new Person("Николай", 0)));
-        //А) получить список уникальных имен
+
+        System.out.println("А) получить список уникальных имен");
         Stream<Person> stream = persons.stream();
         stream.map(Person::getName).distinct().forEach(System.out::println);
 
-        //Б) вывести список уникальных имен в формате: Имена: Иван, Сергей, Петр.
+        System.out.println("Б) вывести список уникальных имен в формате: Имена: Иван, Сергей, Петр.");
         String namesLine = persons.stream().map(Person::getName).distinct().collect(Collectors.joining(", ", "Names: ", "."));
         System.out.println(namesLine);
 
-        //В) получить список людей младше 18, посчитать для них средний возраст
+        System.out.println("В) получить список людей младше 18, посчитать для них средний возраст.");
         Stream<Person> stream1 = persons.stream();
         double averageAgeLess18 = stream1.filter(person -> person.getAge() < 18).mapToDouble(Person::getAge).average().getAsDouble();
         System.out.println("Средний возраст лиц младше 18 - " + averageAgeLess18);
 
-        //Г) при помощи группировки получить Map, в котором ключи – имена, а значения – средний возраст
+        System.out.println("Г) при помощи группировки получить Map, в котором ключи – имена, а значения – средний возраст.");
         Stream<Person> stream2 = persons.stream();
-        Map<String, Double> age = stream2.distinct().collect(Collectors.groupingBy(Person::getName,Collectors.averagingDouble(Person::getAge)));
+        Map<String, Double> age = stream2.distinct().collect(Collectors.groupingBy(Person::getName, Collectors.averagingDouble(Person::getAge)));
         age.forEach((person, p) -> System.out.printf("%s: %s\n", person, p));
 
-        //Д) получить людей, возраст которых от 20 до 45, вывести в консоль их имена в порядке убывания возраста
+        System.out.println("Д) получить людей, возраст которых от 20 до 45, вывести в консоль их имена в порядке убывания возраста");
         Stream<Person> stream3 = persons.stream();
-        Stream<Person> list = stream3.filter(person -> person.getAge() > 20 && person.getAge() < 45).map(Person::getName);
-
+        stream3.filter(person -> person.getAge() > 20 && person.getAge() < 45).sorted((o1, o2) -> o2.getAge() - o1.getAge()).map(Person::getName).forEach(System.out::println);
     }
 }
