6964afef9ade0e9da804fafdccccc88b09efeb9d
Горбунов Петр <40553017+siniirobot@users.noreply.github.com>
2018-08-25 13:56:42 +0700

    А) надо получить именно список уникальных имен. А уже потом печатать +
    Б) надо связать эту задачу с А +
    
    В) warning +-
    
    Г) Плохо использовать \n, надо %n в printf.
    И есть лишнее в коде +-
    
    Д) Надо включительно 20 и 45 +
    
    Общее - когда по цепочке вызываем разные методы stream'ов, то обычно каждый метод ставят на новой строке, см. как в лекции в некоторых местах. +
    
    Е) Надо квадратные корни. +
    И правильнее limit делать в конце, потому что сейчас это просто последовательность чисел.
    А уже потом будто бы отдельно делаются корни.

--- Person/src/ru/academItSchool/gorbunov/main/Realization.java
+++ Person/src/ru/academItSchool/gorbunov/main/Realization.java
@@ -9,50 +9,60 @@ import java.util.stream.*;
 public class Realization {
     public static void main(String[] args) {
         List<Person> persons = new ArrayList<>(Arrays.asList(new Person("Петр", 28),
-                new Person("Петр", 16),
+                new Person("Петр", 19),
                 new Person("Павел", 20),
-                new Person("Сергей", 11),
+                new Person("Сергей", 26),
                 new Person("Сергей", 35),
-                new Person("Александр", 15),
+                new Person("Александр", 29),
                 new Person("Александр", 27),
                 new Person("Алексей", 46),
-                new Person("Николай", 0)));
+                new Person("Николай", 37)));
 
         System.out.println("А) получить список уникальных имен");
-        Stream<Person> stream = persons.stream();
-        stream.map(Person::getName).distinct().forEach(System.out::println);
+        Stream<String> stream = persons.stream()
+                .map(Person::getName)
+                .distinct();
 
-
-        System.out.println();
         System.out.println("Б) вывести список уникальных имен в формате: Имена: Иван, Сергей, Петр.");
-        System.out.println(persons.stream().map(Person::getName).distinct().collect(Collectors.joining(", ", "Names: ", ".")));
-
+        System.out.println(stream.collect(Collectors.joining(", ", "Names: ", ".")));
 
         System.out.println();
         System.out.println("В) получить список людей младше 18, посчитать для них средний возраст.");
-        System.out.println("Средний возраст лиц младше 18 - " + persons.stream().filter(person -> person.getAge() < 18).mapToDouble(Person::getAge).average().getAsDouble());
-
+        System.out.println("Средний возраст лиц младше 18 - " + persons.stream()
+                .filter(person -> person.getAge() < 18)
+                .mapToDouble(Person::getAge)
+                .average()
+                .orElse(0));
 
         System.out.println();
         System.out.println("Г) при помощи группировки получить Map, в котором ключи – имена, а значения – средний возраст.");
-        Map<String, Double> age = persons.stream().distinct().collect(Collectors.groupingBy(Person::getName, Collectors.averagingDouble(Person::getAge)));
-        age.forEach((person, p) -> System.out.printf("%s: %s\n", person, p));
-
+        Map<String, Double> age = persons.stream()
+                .collect(Collectors.groupingBy(Person::getName, Collectors.averagingDouble(Person::getAge)));
+        age.forEach((person, p) -> System.out.printf("%s: %s %n", person, p));
 
         System.out.println();
         System.out.println("Д) получить людей, возраст которых от 20 до 45, вывести в консоль их имена в порядке убывания возраста");
-        System.out.println(persons.stream().filter(person -> person.getAge() > 20 && person.getAge() < 45).sorted((o1, o2) -> o2.getAge() - o1.getAge()).map(Person::getName).collect(Collectors.joining(", старше чем ")));
+        System.out.println(persons.stream()
+                .filter(person -> person.getAge() >= 20 && person.getAge() <= 45)
+                .sorted((o1, o2) -> o2.getAge() - o1.getAge())
+                .map(Person::getName)
+                .collect(Collectors.joining(" старше чем ")));
 
         System.out.println();
         System.out.println("E) создать бесконечный поток корней чисел." + System.lineSeparator() + "С консоли прочитать число – сколько элементов нужно вычислить, затем – распечатать эти элементы");
         System.out.println("Введите колличество корней");
-        DoubleStream squares = DoubleStream.iterate(0, x-> x+1).limit(new Scanner(System.in).nextInt()).map(Math::cbrt);
+        DoubleStream squares = DoubleStream.iterate(0, x -> x + 1)
+                .map(Math::sqrt)
+                .limit(new Scanner(System.in)
+                        .nextInt());
         squares.forEach(System.out::println);
 
         System.out.println();
         System.out.println("Ж)Попробовать реализовать бесконечный поток чисел Фиббоначчи");
         System.out.println("Введите сколько вывести чисел фибоначи: ");
-        LongStream fibonacci = LongStream.iterate(0, x -> x + 1).map(x -> (long) (Math.pow((((Math.sqrt(5)) + 1) / 2), x) / Math.sqrt(5) + 0.5)).limit(new Scanner(System.in).nextInt());
+        LongStream fibonacci = LongStream.iterate(0, x -> x + 1)
+                .map(x -> (long) (Math.pow((((Math.sqrt(5)) + 1) / 2), x) / Math.sqrt(5) + 0.5))
+                .limit(new Scanner(System.in).nextInt());
         fibonacci.forEach(System.out::println);
     }
 }
