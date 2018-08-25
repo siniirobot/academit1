79a128094a5014205b80a9360423a29908111f98
Горбунов Петр <40553017+siniirobot@users.noreply.github.com>
2018-08-25 12:01:05 +0700

    Person Создать бесконечный поток корней чисел. С консоли прочитать число – сколько элементов нужно вычислить, затем – распечатать эти элементы +

--- Person/src/ru/academItSchool/gorbunov/main/Realization.java
+++ Person/src/ru/academItSchool/gorbunov/main/Realization.java
@@ -24,23 +24,31 @@ public class Realization {
         Stream<Person> stream = persons.stream();
         stream.map(Person::getName).distinct().forEach(System.out::println);
 
+
+        System.out.println();
         System.out.println("Б) вывести список уникальных имен в формате: Имена: Иван, Сергей, Петр.");
         System.out.println(persons.stream().map(Person::getName).distinct().collect(Collectors.joining(", ", "Names: ", ".")));
 
+
+        System.out.println();
         System.out.println("В) получить список людей младше 18, посчитать для них средний возраст.");
         System.out.println("Средний возраст лиц младше 18 - " + persons.stream().filter(person -> person.getAge() < 18).mapToDouble(Person::getAge).average().getAsDouble());
 
+
+        System.out.println();
         System.out.println("Г) при помощи группировки получить Map, в котором ключи – имена, а значения – средний возраст.");
         Map<String, Double> age = persons.stream().distinct().collect(Collectors.groupingBy(Person::getName, Collectors.averagingDouble(Person::getAge)));
         age.forEach((person, p) -> System.out.printf("%s: %s\n", person, p));
 
+
+        System.out.println();
         System.out.println("Д) получить людей, возраст которых от 20 до 45, вывести в консоль их имена в порядке убывания возраста");
         System.out.println( persons.stream().filter(person -> person.getAge() > 20 && person.getAge() < 45).sorted((o1, o2) -> o2.getAge() - o1.getAge()).map(Person::getName).collect(Collectors.joining(", старше чем ")));
 
+        System.out.println();
+        System.out.println("E) создать бесконечный поток корней чисел." + System.lineSeparator() + "С консоли прочитать число – сколько элементов нужно вычислить, затем – распечатать эти элементы");
         System.out.println("Введите колличество корней");
         DoubleStream squares = DoubleStream.iterate(0, x-> x+1).limit(new Scanner(System.in).nextInt()).map(Math::cbrt);
         squares.forEach(System.out::println);
-
-
     }
 }
