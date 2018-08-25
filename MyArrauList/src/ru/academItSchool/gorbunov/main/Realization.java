c0dd3bbf12e0199ee552fdc4f89026f8696b51d3
Горбунов Петр <40553017+siniirobot@users.noreply.github.com>
2018-08-25 18:42:06 +0700

    ArrayList проба

--- MyArrauList/src/ru/academItSchool/gorbunov/main/Realization.java
+++ MyArrauList/src/ru/academItSchool/gorbunov/main/Realization.java
@@ -1,4 +1,14 @@
 package ru.academItSchool.gorbunov.main;
 
+import ru.academItSchool.gorbunov.ArrayList.MyArrayList;
+
 public class Realization {
+    public static void main(String[] args) {
+        MyArrayList<String> proba = new MyArrayList<>();
+
+        proba.add("5");
+        proba.add("6");
+
+        System.out.println(proba);
+    }
 }
