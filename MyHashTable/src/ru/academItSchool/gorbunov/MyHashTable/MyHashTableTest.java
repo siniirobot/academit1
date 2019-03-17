e1b07ad6f783a6165f84fa511991636d6516d018
siniirobot <siniirobot@gmail.com>
2019-03-17 15:41:13 +0700

    MyHashTable 1.Правки

---
+++ MyHashTable/src/ru/academItSchool/gorbunov/MyHashTable/MyHashTableTest.java
@@ -0,0 +1,61 @@
+package ru.academItSchool.gorbunov.MyHashTable;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyHashTableTest {
+
+    @Test
+    void size() {
+
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void iterator() {
+    }
+
+    @Test
+    void toArray() {
+    }
+
+    @Test
+    void toArray1() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void containsAll() {
+    }
+
+    @Test
+    void addAll() {
+    }
+
+    @Test
+    void removeAll() {
+    }
+
+    @Test
+    void retainAll() {
+    }
+
+    @Test
+    void clear() {
+    }
+}
