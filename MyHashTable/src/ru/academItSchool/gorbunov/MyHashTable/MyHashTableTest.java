120911efe76d5f14243e06343d8b58340d94a4f3
siniirobot <siniirobot@gmail.com>
2019-03-24 12:49:38 +0700

    MyHashTable 1. Начал работу над тестами

---
+++ MyHashTable/src/ru/academItSchool/gorbunov/MyHashTable/MyHashTableTest.java
@@ -0,0 +1,61 @@
+package ru.academItSchool.gorbunov.MyHashTable;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class MyHashTableTest {
+
+    @Test
+    public void testSize() {
+    }
+
+    @Test
+    public void testIsEmpty() {
+    }
+
+    @Test
+    public void testContains() {
+    }
+
+    @Test
+    public void testIterator() {
+    }
+
+    @Test
+    public void testToArray() {
+    }
+
+    @Test
+    public void testToArray1() {
+    }
+
+    @Test
+    public void testAdd() {
+    }
+
+    @Test
+    public void testRemove() {
+    }
+
+    @Test
+    public void testContainsAll() {
+    }
+
+    @Test
+    public void testAddAll() {
+    }
+
+    @Test
+    public void testRemoveAll() {
+    }
+
+    @Test
+    public void testRetainAll() {
+    }
+
+    @Test
+    public void testClear() {
+    }
+}
