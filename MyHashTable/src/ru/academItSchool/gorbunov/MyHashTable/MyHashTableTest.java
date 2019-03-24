68a83f6b21ca982ae73f93d930e05c54f034d521
siniirobot <siniirobot@gmail.com>
2019-03-24 12:59:39 +0700

    MyHashTable 1. Тест size

--- MyHashTable/src/ru/academItSchool/gorbunov/MyHashTable/MyHashTableTest.java
+++ MyHashTable/src/ru/academItSchool/gorbunov/MyHashTable/MyHashTableTest.java
@@ -6,9 +6,20 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
 public class MyHashTableTest {
+    @DataProvider(name = "Size")
+    public Object[][] size() {
+        MyHashTable<String> hashTable = new MyHashTable<>();
+        hashTable.add("1");
+        hashTable.add("2");
+        hashTable.add("3");
+        return new Object[][]{
+                new Object[]{hashTable, 3}
+        };
+    }
 
-    @Test
-    public void testSize() {
+    @Test (dataProvider = "Size")
+    public void testSize(MyHashTable table, int result) {
+        assertEquals(table.size(),result);
     }
 
     @Test
