320f943752d83e6aae6c92c1fa4bdf660c2419b0
siniirobot <siniirobot@gmail.com>
2019-04-01 22:04:42 +0700

    MyArrayList 1. Правки

--- MyArrayList/src/ru/academItSchool/gorbunov/myArrayList/MyArrayList.java
+++ MyArrayList/src/ru/academItSchool/gorbunov/myArrayList/MyArrayList.java
@@ -546,8 +546,9 @@ public class MyArrayList<T> implements List<T> {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
+        result = prime * result + Integer.hashCode(this.count);
         for (int i = 0; i < this.count; i++) {
-            result = prime * result + this.listElements[i].hashCode();
+            result = prime * result + Objects.hashCode(this.listElements[i]);
         }
         return result;
     }
