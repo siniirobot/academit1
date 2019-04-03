bbc61b5a1e7813fa0e1b075ad95c02a255f23cf4
siniirobot <siniirobot@gmail.com>
2019-04-03 10:11:58 +0700

    MyHashTable 1.Метод Clear

--- MyHashTable/src/ru/academItSchool/gorbunov/MyHashTable/MyHashTable.java
+++ MyHashTable/src/ru/academItSchool/gorbunov/MyHashTable/MyHashTable.java
@@ -261,7 +261,14 @@ public class MyHashTable<T> implements Collection<T> {
 
     @Override
     public void clear() {
-
+        for (int i = 0; i < this.array.length; i++) {
+            if (this.array[i] == null) {
+                continue;
+            }
+            this.array[i] = null;
+        }
+        this.count = 0;
+        this.modCount++;
     }
 
     @Override
