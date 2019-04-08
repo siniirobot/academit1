bb75d2b20c9cc4d55368089e5c11646229793083
siniirobot <siniirobot@gmail.com>
2019-04-08 10:16:11 +0700

    MyBinaryTree 1. Создал класс листа  1.1 Создал конструктор пустой  1.2 Создал конструктор с еще листом  1.3 создалкласс компоратора  1.4 создал геттер и сеттер

---
+++ MyBinaryTree/src/ru/academItSchool/gorbunov/BinaryTree/BinaryTree.java
@@ -0,0 +1,65 @@
+package ru.academItSchool.gorbunov.BinaryTree;
+
+import java.util.Comparator;
+
+public class BinaryTree {
+    private class MyTreeNode<T> {
+        private MyTreeNode<T> left;
+        private MyTreeNode<T> right;
+        private T data;
+
+        public MyTreeNode() {
+        }
+
+        public MyTreeNode(T data) {
+            this.data = data;
+        }
+
+        public MyTreeNode(T data, MyTreeNode<T> node) {
+            this.data = data;
+            if (new CompareForTreeNode().compare(this.data,node.data) < 0) {
+                this.left = node;
+                this.right = null;
+            }else {
+                this.left = null;
+                this.right = node;
+            }
+        }
+
+        public MyTreeNode(T data, MyTreeNode<T> leftNode, MyTreeNode<T> rightNode) {
+            this.data = data;
+            this.right = rightNode;
+            this.left = leftNode;
+        }
+
+        public class CompareForTreeNode implements Comparator<T> {
+            public int compare(T val1, T val2) {
+                return ((Comparable<? super T>) val1).compareTo(val2);
+            }
+        }
+
+        public MyTreeNode<T> getLeft() {
+            return left;
+        }
+
+        public void setLeft(MyTreeNode<T> left) {
+            this.left = left;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        public MyTreeNode<T> getRight() {
+            return right;
+        }
+
+        public void setRight(MyTreeNode<T> right) {
+            this.right = right;
+        }
+    }
+}
