93f25302c24c80ab1f3d2cf698e9d26a9720f299
siniirobot <siniirobot@gmail.com>
2019-04-20 11:10:58 +0700

    InputOutputStreams 1.Начало

---
+++ InputOutputStreams/src/ru.academItSchool.gorbunov/main/InputOutputStreams.java
@@ -0,0 +1,9 @@
+package ru.academItSchool.gorbunov.main;
+
+import java.io.IOException;
+
+public class InputOutputStreams {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
