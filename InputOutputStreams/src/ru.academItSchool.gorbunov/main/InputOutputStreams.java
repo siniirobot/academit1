12e6ac15df248769cc2a915ea47f9ba9f2d4e07d
sibiirobot <siniirobot@GMAIL.COM>
2019-04-20 12:32:52 +0700

    Merge branch 'master' of https://github.com/siniirobot/academit1
    # Conflicts:
    #	MyBinaryTree/src/ru/academItSchool/gorbunov/BinaryTree/BinaryTree.java

--- InputOutputStreams/src/ru.academItSchool.gorbunov/main/InputOutputStreams.java
+++ InputOutputStreams/src/ru.academItSchool.gorbunov/main/InputOutputStreams.java
@@ -1,9 +1,21 @@
 package ru.academItSchool.gorbunov.main;
 
-import java.io.IOException;
+import java.io.*;
 
 public class InputOutputStreams {
     public static void main(String[] args) throws IOException {
-
+        try (BufferedInputStream inputStream =
+                     new BufferedInputStream(
+                             new FileInputStream("ГарриПоттер.txt"));
+             BufferedOutputStream bufferedOutputStream =
+                     new BufferedOutputStream(
+                             new FileOutputStream("ГарриКопия"))) {
+            int read = 0;
+            int off = 0;
+            byte[] res = new byte[1000000];
+            while ((read = inputStream.read(res,off,res.length - off)) != -1) {
+                off += read;
+            }
+        }
     }
 }
