67168a3764e7cb5d8acc45ec24d52c754e703197
sibiirobot <siniirobot@GMAIL.COM>
2019-04-21 10:23:45 +0700

    InputOut 1.?

--- InputOutputStreams/src/ru.academItSchool.gorbunov/main/InputOutputStreams.java
+++ InputOutputStreams/src/ru.academItSchool.gorbunov/main/InputOutputStreams.java
@@ -6,16 +6,17 @@ public class InputOutputStreams {
     public static void main(String[] args) throws IOException {
         try (BufferedInputStream inputStream =
                      new BufferedInputStream(
-                             new FileInputStream("ГарриПоттер.txt"));
+                             new FileInputStream("Властелин колец.txt"));
              BufferedOutputStream bufferedOutputStream =
                      new BufferedOutputStream(
-                             new FileOutputStream("ГарриКопия"))) {
+                             new FileOutputStream("ГарриКопия.txt"))) {
             int read = 0;
             int off = 0;
             byte[] res = new byte[1000000];
-            while ((read = inputStream.read(res,off,res.length - off)) != -1) {
+            while ((read = inputStream.read(res, off, res.length - off)) != -1) {
                 off += read;
             }
+            bufferedOutputStream.write(res);
         }
     }
 }
