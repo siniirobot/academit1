6815931eee074abfb98e076f4eefe0c390d822d5
siniirobot <siniirobot@gmail.com>
2019-04-22 10:36:07 +0700

    InputOutStreams 1.Сделал первое задание 2.Сделал второе задание

--- InputOutputStreams/src/ru.academItSchool.gorbunov/main/InputOutputStreams.java
+++ InputOutputStreams/src/ru.academItSchool.gorbunov/main/InputOutputStreams.java
@@ -9,15 +9,21 @@ public class InputOutputStreams {
                              new FileInputStream("ГарриПоттер.txt"));
              BufferedOutputStream bufferedOutputStream =
                      new BufferedOutputStream(
-                             new FileOutputStream("ГарриКопия.txt"))) {
-            int read = 0;
-            int off = 0;
-            byte[] res = new byte[10];
-            while ((read = inputStream.read(res, off, res.length - off)) != -1) {
-                off += read;
-                bufferedOutputStream.write(res);
+                             new FileOutputStream("ГарриКопия.txt"));
+             PrintWriter out = new PrintWriter("Строки.txt")) {
+            int read;
+            byte[] res = new byte[1000000];
+            while ((read = inputStream.read(res)) != -1) {
+                bufferedOutputStream.write(res,0,read);
                 bufferedOutputStream.flush();
             }
+
+            for (int i = 1; i <= 100; i++ ) {
+                out.println("Строка " + i);
+            }
+            out.print("Это последняя строка.");
+            out.println("Но это еще не конец.");
+            out.printf("Тут %d строка",101);
         }
     }
 }
