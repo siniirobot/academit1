6f2feb9338919dfd5b61f3770b7cb13ce7842188
siniirobot <siniirobot@gmail.com>
2019-04-22 10:37:39 +0700

    InputOutStreams 1.Правки

--- InputOutputStreams/src/ru.academItSchool.gorbunov/main/InputOutputStreams.java
+++ InputOutputStreams/src/ru.academItSchool.gorbunov/main/InputOutputStreams.java
@@ -14,16 +14,16 @@ public class InputOutputStreams {
             int read;
             byte[] res = new byte[1000000];
             while ((read = inputStream.read(res)) != -1) {
-                bufferedOutputStream.write(res,0,read);
+                bufferedOutputStream.write(res, 0, read);
                 bufferedOutputStream.flush();
             }
 
-            for (int i = 1; i <= 100; i++ ) {
+            for (int i = 1; i <= 100; i++) {
                 out.println("Строка " + i);
             }
             out.print("Это последняя строка.");
             out.println("Но это еще не конец.");
-            out.printf("Тут %d строка",101);
+            out.printf("Тут %d строка", 101);
         }
     }
 }
