d4cc4ece423fd7e3a12a137d6d6f4bbff71f5163
sibiirobot <siniirobot@GMAIL.COM>
2019-05-04 22:38:40 +0700

    Serializable 1. Начало

---
+++ Serializable/src/ru.academItSchool.gorbunov/MyClass/MyClass.java
@@ -0,0 +1,11 @@
+package ru.academItSchool.gorbunov.MyClass;
+
+import java.io.Serializable;
+
+public class MyClass implements Serializable {
+    private int[][] matrix;
+
+    public MyClass(int[][] matrix) {
+
+    }
+}
