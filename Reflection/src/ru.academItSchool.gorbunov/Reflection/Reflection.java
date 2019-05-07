3ed0a450b34a2d0c2bd1484bb4d056f8acaca1c7
siniirobot <siniirobot@gmail.com>
2019-05-07 14:11:16 +0700

    Reflection 1. Поправил строки

--- Reflection/src/ru.academItSchool.gorbunov/Reflection/Reflection.java
+++ Reflection/src/ru.academItSchool.gorbunov/Reflection/Reflection.java
@@ -16,21 +16,24 @@ public class Reflection {
             InstantiationException, NoSuchFieldException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите название класса с которым хотите работать.");
+
         String className = scanner.nextLine();
         Class a = Class.forName("ru.academItSchool.gorbunov.Classes." + className);
+
         className = scanner.nextLine();
         Class b = Class.forName("ru.academItSchool.gorbunov.Classes." + className);
-        Constructor constructorA = a.getConstructor(int.class,String.class);
-        ClassA classA = (ClassA)constructorA.newInstance(3,"Пень");
-        Constructor constructorB = b.getConstructor(int.class,String.class);
-        ClassB classB = (ClassB) constructorB.newInstance(2,"Intel");
+
+        Constructor constructorA = a.getConstructor(int.class, String.class);
+        ClassA classA = (ClassA) constructorA.newInstance(3, "Пень");
+        Constructor constructorB = b.getConstructor(int.class, String.class);
+        ClassB classB = (ClassB) constructorB.newInstance(2, "Intel");
 
         Field fieldA = a.getField("a");
         fieldA.setAccessible(true);
         Field fieldB = classB.getClass().getDeclaredField("b");
         fieldB.setAccessible(true);
-        fieldA.set(classA,25);
-        fieldB.set(classB,"Селерон");
+        fieldA.set(classA, 25);
+        fieldB.set(classB, "Селерон");
 
         Method methodClassA = a.getMethod("getA");
         System.out.println(methodClassA.invoke(classA));
