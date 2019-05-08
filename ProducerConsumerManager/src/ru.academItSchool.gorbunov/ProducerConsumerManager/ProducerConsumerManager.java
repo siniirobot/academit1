48e2eae2cfcb88fd1c31c414b0e318cc25530c76
siniirobot <siniirobot@gmail.com>
2019-05-08 09:34:50 +0700

    ProducerConsumerManager 1.Условие задачи:   1 Есть несколько потоков-производителей. Они выполняют   некоторую работу, и кладут результат в список   определенной вместимости+   2 Есть несколько потоков-потребителей. Они ждут когда в   этом списке что-то появится, и забирают результат оттуда по   одному элементу+   3 Необходимо реализовать это взаимодействие потоков, и   чтобы потоки были правильно синхронизованы+

---
+++ ProducerConsumerManager/src/ru.academItSchool.gorbunov/ProducerConsumerManager/ProducerConsumerManager.java
@@ -0,0 +1,68 @@
+package ru.academItSchool.gorbunov.ProducerConsumerManager;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ProducerConsumerManager {
+    private int producersCount;
+    private int consumerCount;
+    private final Queue<String> queue = new LinkedList<>();
+    private static final int MAX_CAPACITY = 20;
+
+    public ProducerConsumerManager(int producersCount, int consumerCount) {
+        this.producersCount = producersCount;
+        this.consumerCount = consumerCount;
+    }
+
+    public void start() {
+        for (int i = 0; i <= this.producersCount; i++) {
+            Thread t = new Thread(()->{
+               try {
+                   int j = 1;
+
+                   while (true) {
+                       String el = "Элемент" + j;
+                       j++;
+                       Thread.sleep(2000);
+
+                       synchronized (queue) {
+                           while (queue.size() >= MAX_CAPACITY) {
+                               queue.wait();
+                           }
+
+                           queue.offer(el);
+                           System.out.println("Размер очереди производителя - " + queue.size());
+                           queue.notifyAll();
+                       }
+                   }
+               }catch (InterruptedException ignored){
+               }
+            });
+            t.start();
+        }
+
+        for (int i = 0; i <= this.consumerCount; i++) {
+            Thread t = new Thread(()->{
+                try {
+                    while (true) {
+                        Thread.sleep(2000);
+
+                        synchronized (queue) {
+                            while (queue.isEmpty()) {
+                                queue.wait();
+                            }
+
+                            String el = queue.remove();
+                            System.out.println("Размер очереди потребителя - " + queue.size());
+                            System.out.println(el);
+                            queue.notifyAll();
+                        }
+                    }
+                }catch (InterruptedException ignored){
+
+                }
+            });
+            t.start();
+        }
+    }
+}
---
+++ ProducerConsumerManager/src/ru.academItSchool.gorbunov/main/main.java
@@ -0,0 +1,10 @@
+package ru.academItSchool.gorbunov.main;
+
+import ru.academItSchool.gorbunov.ProducerConsumerManager.ProducerConsumerManager;
+
+public class main {
+    public static void main(String[] args) {
+        ProducerConsumerManager producerConsumerManager = new ProducerConsumerManager(3,1);
+        producerConsumerManager.start();
+    }
+}
