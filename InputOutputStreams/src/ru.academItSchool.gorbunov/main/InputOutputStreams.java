1b88ac038f2f5156a82500ade55ee5618704ee51
siniirobotRab <siniirobot@gmail.com>
2019-05-14 09:15:33 +0700

    InputOutputStreams 1. Надо убрать лишние пустые строки: http://joxi.ru/4AkOwPeioVPBGA+ 2. flush лучше делать в конце.+ Либо можно не делать, т.к. потом всё закроется, и само сделается 3. Лучше обрабатывать исключения+

--- InputOutputStreams/src/ru.academItSchool.gorbunov/main/InputOutputStreams.java
+++ InputOutputStreams/src/ru.academItSchool.gorbunov/main/InputOutputStreams.java
@@ -4,28 +4,27 @@ import java.io.*;
 
 public class InputOutputStreams {
     public static void main(String[] args) throws IOException {
-        try (BufferedInputStream inputStream =
-                     new BufferedInputStream(
-                             new FileInputStream("ГарриПоттер.txt"));
-             BufferedOutputStream bufferedOutputStream =
-                     new BufferedOutputStream(
-                             new FileOutputStream("ГарриКопия.txt"));
+        try (BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream("ГарриПоттер.txt"));
+             BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream("ГарриКопия.txt"));
              PrintWriter out = new PrintWriter("Строки.txt")) {
             int read;
             byte[] res = new byte[1000000];
-
-            while ((read = inputStream.read(res)) != -1) {
-                bufferedOutputStream.write(res, 0, read);
+            try{
+                while ((read = inputStream.read(res)) != -1) {
+                    bufferedOutputStream.write(res, 0, read);
+                }
                 bufferedOutputStream.flush();
+                for (int i = 1; i <= 100; i++) {
+                    out.println("Строка " + i);
+                }
+
+                out.print("Это последняя строка.");
+                out.println("Но это еще не конец.");
+                out.printf("Тут %d строка", 101);
+            }catch (IOException e) {
+                System.out.println(e);
             }
 
-            for (int i = 1; i <= 100; i++) {
-                out.println("Строка " + i);
-            }
-
-            out.print("Это последняя строка.");
-            out.println("Но это еще не конец.");
-            out.printf("Тут %d строка", 101);
         }
     }
 }
