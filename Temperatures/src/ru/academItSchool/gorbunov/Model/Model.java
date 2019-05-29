29facccd836a3d5f300eefb64985b124804f1c20
siniirobotRab <siniirobot@gmail.com>
2019-05-29 08:25:33 +0700

    Temperatures:
    1. Можно было сделать проще - метод changeTemparatureTo заменить на метод fromCelsius с 1 аргументом - температурой.
    Это можно не исправлять+
    
    2. Сейчас окно появляется в верхнем левом углу.
    Лучше, чтобы появлялось по центру экрана
    
    3. Результат плохо выводится:
    http://joxi.ru/n2Yz7N5TbyqKoA
    
    4. getStringChar - не очень хорошее имя.
    Лучше дать более простое и понятное имя.
    Например, getScaleSymbol.+
    
    Аналогично getToStringArray не очень хорошее имя+
    
    5. При работе со stream лучше писать по 1 команде в строке+
    
    6. View:
    - warning
    - в коде не должно быть 2 пустых строки подряд
    - лишняя пустая строка в начале класса
    
    7. Модель и контроллер уже должны работать не со строками.
    И не должны валидировать пользовательский ввод.
    Получением данных от пользователя и преобразованием их в нужный вид должна заниматься view
    
    8. От контроллера здесь по факту нет пользы, его можно убрать
    
    9. Лучше не создавать модель каждый раз заново, а сделать, чтобы она была одна

--- Temperatures/src/ru/academItSchool/gorbunov/Model/Model.java
+++ Temperatures/src/ru/academItSchool/gorbunov/Model/Model.java
@@ -21,12 +21,18 @@ public class Model {
     }
 
     private TemperatureConverter getScale(String character) {
-        return Arrays.stream(this.temperatureConversions)
-                .filter(x -> x.getScaleChar().equals(character)).findFirst().orElse(null);
+        return Arrays
+                .stream(this.temperatureConversions)
+                .filter(x -> x.getScaleChar().equals(character))
+                .findFirst()
+                .orElse(null);
     }
 
     public String[] getScaleArray() {
-        return Arrays.stream(this.temperatureConversions).map(TemperatureConverter::getScaleChar).toArray(String[]::new);
+        return Arrays
+                .stream(this.temperatureConversions)
+                .map(TemperatureConverter::getScaleChar)
+                .toArray(String[]::new);
     }
 
     public String changeTemperature(String temperature, String from, String to) {
