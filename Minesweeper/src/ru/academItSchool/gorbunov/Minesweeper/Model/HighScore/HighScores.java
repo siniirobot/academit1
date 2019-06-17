68892a98bc0bea584beda0f131e3ec74ee1e893a
siniirobotRab <siniirobot@gmail.com>
2019-06-17 13:54:09 +0700

    Minesweeper: 1. Добавил описания методов Таблицы рекордов.

--- Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper/Model/HighScore/HighScores.java
+++ Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper/Model/HighScore/HighScores.java
@@ -7,7 +7,11 @@ import java.io.*;
 import static ru.academItSchool.gorbunov.Minesweeper.Model.Difficult.Difficult.nameDifficult.*;
 
 public class HighScores implements Serializable {
-
+    /**
+     * Преобразует имя сложности в имя файла.
+     * @param difficult сложность игрока
+     * @return имя файла
+     */
     private String getFileName(Difficult.nameDifficult difficult) {
         String file = "";
 
@@ -26,14 +30,33 @@ public class HighScores implements Serializable {
         return file;
     }
 
+    /**
+     * Открывает файл с пмомщью полученого ранее имя файла для сериализацмм.
+     * @param difficult имя сложности игрока
+     * @return поток с открытым файлом
+     * @throws IOException
+     */
     private ObjectInputStream openFile(String difficult) throws IOException {
         return new ObjectInputStream(new FileInputStream(difficult));
     }
 
+    /**
+     * Закрывет файл с сериализуемым объектом
+     * @param difficult имя сложности игрока
+     * @return поток с закрытием файла
+     * @throws IOException
+     */
     private ObjectOutputStream closeFile(String difficult) throws IOException {
         return new ObjectOutputStream(new FileOutputStream(difficult));
     }
 
+    /**
+     * Открывает файл ,десириализует объект из потока(В случае отсутсвия файла создает его и записывает игрока
+     * первым в таблице.) Если добавляемый игрок по времени окозался хуже чем остальные игроки то выдается исключение
+     * IllegalArgumentException, в противном случае идет перебор игроков до тех пор пока результат игрока не займет
+     * свое место. После чего объект сериализуется и записывается заново.
+     * @param player Игрок
+     */
     public void add(Player player) {
         Player[] highScores = new Player[10];
         String fileName = getFileName(player.getDifficult());
@@ -75,6 +98,10 @@ public class HighScores implements Serializable {
         }
     }
 
+    /**
+     * Печатает таблицу рекордов определенной сложности.
+     * @param difficult сложность.
+     */
     public void printHighScores(Difficult difficult) {
         String fileName = getFileName(difficult.getName());
 
