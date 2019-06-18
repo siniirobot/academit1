d09fb6bdcd074b4361f8708515d87cc105fa5332
siniirobotRab <siniirobot@gmail.com>
2019-06-18 15:56:19 +0700

    Minesweeper: 1.Добавид описание исключений

--- Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper/Model/Exceptions/Boom.java
+++ Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper/Model/Exceptions/Boom.java
@@ -1,6 +1,10 @@
 package ru.academItSchool.gorbunov.Minesweeper.Model.Exceptions;
 
 public class Boom extends Exception {
+    /**
+     * Маркер выхода из игры путем взрыва.
+     * @param message - сообщение выхода.
+     */
     public Boom(String message) {
         super(message);
     }
--- Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper/Model/Exceptions/EndGame.java
+++ Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper/Model/Exceptions/EndGame.java
@@ -1,6 +1,10 @@
 package ru.academItSchool.gorbunov.Minesweeper.Model.Exceptions;
 
 public class EndGame extends Exception {
+    /**
+     * Маркер выхода из игры.
+     * @param message - сообщение.
+     */
     public EndGame(String message) {
         super(message);
     }
