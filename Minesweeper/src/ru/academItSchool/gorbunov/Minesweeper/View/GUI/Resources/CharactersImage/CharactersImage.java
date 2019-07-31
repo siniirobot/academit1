eea322eae7ea1a28222cb9408e3232a7364cd195
siniirobotRab <siniirobot@gmail.com>
2019-07-31 11:47:37 +0700

    Minesweeper:  1.Правки

--- Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper/View/GUI/Resources/CharactersImage/CharactersImage.java
+++ Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper/View/GUI/Resources/CharactersImage/CharactersImage.java
@@ -13,32 +13,32 @@ public class CharactersImage implements Characters {
     public CharactersImage() {
         try {
             this.characters = new Object[]{
-                    ImageIO.read(new File("Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper" +
-                            "/View/GUI/Resources/CharactersImage/openCell.png")),
-                    ImageIO.read(new File("Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper" +
-                            "/View/GUI/Resources/CharactersImage/one.png")),
-                    ImageIO.read(new File("Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper" +
-                            "/View/GUI/Resources/CharactersImage/two.png")),
-                    ImageIO.read(new File("Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper" +
-                            "/View/GUI/Resources/CharactersImage/three.png")),
-                    ImageIO.read(new File("Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper" +
-                            "/View/GUI/Resources/CharactersImage/four.png")),
-                    ImageIO.read(new File("Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper" +
-                            "/View/GUI/Resources/CharactersImage/five.png")),
-                    ImageIO.read(new File("Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper" +
-                            "/View/GUI/Resources/CharactersImage/six.png")),
-                    ImageIO.read(new File("Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper" +
-                            "/View/GUI/Resources/CharactersImage/seven.png")),
-                    ImageIO.read(new File("Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper" +
-                            "/View/GUI/Resources/CharactersImage/eight.png")),
-                    ImageIO.read(new File("Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper" +
-                            "/View/GUI/Resources/CharactersImage/closeCell.png")),
-                    ImageIO.read(new File("Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper" +
-                            "/View/GUI/Resources/CharactersImage/mine.png")),
-                    ImageIO.read(new File("Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper" +
-                            "/View/GUI/Resources/CharactersImage/flag.png")),
-                    ImageIO.read(new File("Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper" +
-                            "/View/GUI/Resources/CharactersImage/questionMark.png")),
+                    new ImageIcon(ImageIO.read(new File("Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper" +
+                            "/View/GUI/Resources/CharactersImage/openCell.png"))),
+                    new ImageIcon(ImageIO.read(new File("Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper" +
+                            "/View/GUI/Resources/CharactersImage/one.png"))),
+                    new ImageIcon(ImageIO.read(new File("Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper" +
+                            "/View/GUI/Resources/CharactersImage/two.png"))),
+                    new ImageIcon(ImageIO.read(new File("Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper" +
+                            "/View/GUI/Resources/CharactersImage/three.png"))),
+                    new ImageIcon(ImageIO.read(new File("Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper" +
+                            "/View/GUI/Resources/CharactersImage/four.png"))),
+                    new ImageIcon(ImageIO.read(new File("Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper" +
+                            "/View/GUI/Resources/CharactersImage/five.png"))),
+                    new ImageIcon(ImageIO.read(new File("Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper" +
+                            "/View/GUI/Resources/CharactersImage/six.png"))),
+                    new ImageIcon(ImageIO.read(new File("Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper" +
+                            "/View/GUI/Resources/CharactersImage/seven.png"))),
+                    new ImageIcon(ImageIO.read(new File("Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper" +
+                            "/View/GUI/Resources/CharactersImage/eight.png"))),
+                    new ImageIcon(ImageIO.read(new File("Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper" +
+                            "/View/GUI/Resources/CharactersImage/closeCell.png"))),
+                    new ImageIcon(ImageIO.read(new File("Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper" +
+                            "/View/GUI/Resources/CharactersImage/mine.png"))),
+                    new ImageIcon(ImageIO.read(new File("Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper" +
+                            "/View/GUI/Resources/CharactersImage/flag.png"))),
+                    new ImageIcon(ImageIO.read(new File("Minesweeper/src/ru/academItSchool/gorbunov/Minesweeper" +
+                            "/View/GUI/Resources/CharactersImage/questionMark.png"))),
             };
         } catch (IOException e) {
             e.printStackTrace();
